ed377f039d7cb808ee6c6bd14f98b6e1f5d6ce7a
Aditya Rathour <adityarajpootrathour@gmail.com>
2026-04-26 18:26:41 +0530

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Skip ESLint errors during Vercel build
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
